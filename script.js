4987d6bc1c718928e9a7685e1ea5ab86857c875e
B K R V V Durga Prasad <b2kodandaram@gmail.com>
2025-06-13 11:15:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,51 @@
-//complete this code
-class Person {}
+// Person class
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  // Getter for name
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  // Getter for age
+  get age() {
+    return this._age;
+  }
 
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+  // Setter for age
+  set age(age) {
+    this._age = age;
+  }
+}
+
+// Student class extends Person
+class Student extends Person {
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+// Teacher class extends Person
+class Teacher extends Person {
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
+
+// Creating a Person instance
+const person = new Person("John", 25);
+console.log(person.name);    // Output: John
+
+person.age = 30;             // Using setter
+console.log(person.age);     // Output: 30
+
+// Creating a Student instance
+const student = new Student("Alice", 22);
+student.study();             // Output: Alice is studying
+
+// Creating a Teacher instance
+const teacher = new Teacher("Bob", 40);
+teacher.teach();             // Output: Bob is teaching
